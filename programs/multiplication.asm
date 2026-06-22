a56455a33beb54a0ed1d196dbcc58ca2bed19ab3
Michael <mihalishatzoglakis@gmail.com>
2026-06-22 15:26:29 +0300

    Added Labels to multiplication.asm

--- programs/multiplication.asm
+++ programs/multiplication.asm
@@ -3,26 +3,26 @@
 LDI 00
 STA FD     // FD holds the final result
 
-LDI 05     // Multiplier (A)
+LDI 05     // Multiplier
 STA FF
-LDI 03     // Multiplicand (B)
+LDI 03     // Multiplicand
 STA FE
 
-//LOOP START (starts at byte 0C)
+loop:
 LDA FF
 CMPI 00
-JEQ 2A     //jump at the end if one
+JEQ exit     //jump at the end if one
 
 ANDI 01    // AND A with 1. (If A is odd, result is 1. If even, result is 0)
 CMPI 00
-JEQ 1E     // If the result was 0 (A is even), skip the addition
+JEQ shifts     // If the result was 0 (A is even), skip the addition
 
 //ADDITION
 LDA FD
 ADD FE
 STA FD
 
-//SHIFTS (starts at byte 1E)
+shifts:
 LDA FE
 SLL
 STA FE
@@ -31,8 +31,9 @@ LDA FF
 SRL
 STA FF
 
-JMP 0C
+JMP loop
 
+exit:
 //OUTPUT RESULT
 LDA FD
 OUT
